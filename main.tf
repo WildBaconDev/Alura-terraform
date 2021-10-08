b8898259d237d875ee3ccec48bc9c9320ecf79d2
Davi Miranda Gonçalves <davi.mirandagonc@gmail.com>
2021-10-07 21:10:15 -0300

    Aula 4

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "aws" {
+    alias = "us-east-2"
+    region = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
     count = 3
     ami = "ami-09e67e426f25ce0d7"
@@ -53,34 +58,45 @@ resource "aws_instance" "dev5" {
     vpc_security_group_ids = [ "${aws_security_group.acesso-ssh.id}" ]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress = [
-    {
-        description      = "acesso-ssh"
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["189.6.118.158/32"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = false
+resource "aws_instance" "dev6" {
+    provider = aws.us-east-2
+    ami = "ami-074cce78125f09d61"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+      "Name" = "dev6"
     }
-  ]
 
-  tags = {
-    Name = "ssh"
-  }
+    vpc_security_group_ids = [ aws_security_group.acesso-ssh-us-east-2.id ]
+    
+    depends_on = [
+      aws_dynamodb_table.dynamodb-homologacao
+    ]
 }
 
 resource "aws_s3_bucket" "dev4" {
-  bucket = "davimirandalabs-dev4"
-  acl    = "private"
+    bucket = "davimirandalabs-dev4"
+    acl    = "private"
 
-  tags = {
+    tags = {
     Name = "davimirandalabs-dev4"
-  }
+    }
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+    provider = aws.us-east-2
+    name           = "GameScores"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "UserId"
+    range_key      = "GameTitle"
+
+    attribute {
+    name = "UserId"
+    type = "S"
+    }
+
+    attribute {
+    name = "GameTitle"
+    type = "S"
+    }
 }
---
+++ security-group.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress = [
+    {
+        description      = "acesso-ssh"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["189.6.118.158/32"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        security_groups = []
+        self = false
+    }
+  ]
+
+  tags = {
+    Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+    provider = aws.us-east-2
+    name        = "acesso-ssh"
+    description = "acesso-ssh"
+
+    ingress = [
+        {
+            description      = "acesso-ssh"
+            from_port        = 22
+            to_port          = 22
+            protocol         = "tcp"
+            cidr_blocks      = ["189.6.118.158/32"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = false
+        }
+    ]
+
+    tags = {
+        Name = "ssh"
+    }
+}
